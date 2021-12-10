fe8628e8315945784112dd7d8d21a42c51b7e259
Jan S <32067841+AsgrimS@users.noreply.github.com>
2021-12-10 21:11:02 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 
-pacman -S --needed linux linux-lts linux-firmware linux-headers linux-lts-headers btrfs-progs intel-ucode base-devel sof-firmware alsa-ucm-conf openssh networkmanager dialog wireless_tools grub dosfstools rsync reflector os-prober mtools efibootmgr bluez bluez-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack cups man-db
+pacman -S --needed linux linux-lts linux-firmware linux-headers linux-lts-headers btrfs-progs intel-ucode base-devel sof-firmware alsa-ucm-conf openssh networkmanager dialog wireless_tools grub dosfstools rsync reflector os-prober mtools efibootmgr bluez bluez-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack avahi cups man-db
 pacman -S tlp # You can remove the tlp package if you are installing on a desktop or vm
 
 # pacman -S xf86-video-amdgpu # Drivers for AMD
@@ -27,6 +27,7 @@ grub-mkconfig -o /boot/grub/grub.cfg
 systemctl enable NetworkManager
 systemctl enable bluetooth.service
 systemctl enable cups.service
+systemctl enable avahi-daemon
 systemctl enable sshd
 systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
 systemctl enable reflector.timer
