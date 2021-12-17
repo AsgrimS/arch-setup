9f1e59e97745124c4dc39194e87947e759c73ed7
Jan S <32067841+AsgrimS@users.noreply.github.com>
2021-12-17 12:31:42 +0100

    fonts update

--- post-installation-tweaks.sh
+++ post-installation-tweaks.sh
@@ -4,7 +4,7 @@ sudo timedatectl set-ntp true
 sudo reflector --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
 
 paru -S gnome gnome-tweaks # Gnome DE
-paru -S --needed ttf-fira-code ttf-ubuntu-font-family noto-fonts-emoji matcha-gtk-theme xcursor-breeze papirus-icon-theme # Optional theme/fonts packages
+paru -S --needed ttf-fira-code ttf-ubuntu-font-family noto-fonts-emoji noto-fonts noto-fonts-cjk matcha-gtk-theme xcursor-breeze papirus-icon-theme # Optional theme/fonts packages
 paru -S zramd
 
 sudo systemctl enable gdm # For gnome
